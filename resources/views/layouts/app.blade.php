482a7fbe04a7f41559de41111cbafebde5cea16a
ppociecha <pa.pociecha@gmail.com>
2026-03-28 11:00:04 +0100

    main

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,16 +4,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title', __('site.name'))</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;700&display=swap" rel="stylesheet">
-    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    @else
-        <script src="https://cdn.tailwindcss.com"></script>
-    @endif
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body class="min-h-screen bg-[#f1f4f9] text-[#003174] antialiased font-['Montserrat']">
+<body class="min-h-screen bg-[#f1f4f9] font-sans text-[#003174] antialiased">
     <a href="#main" class="sr-only focus:not-sr-only focus:absolute focus:left-4 focus:top-4 focus:z-50 focus:rounded focus:bg-white focus:px-3 focus:py-2 focus:shadow">{{ __('nav.skip') }}</a>
     @include('partials.header')
     <main id="main" class="w-full py-0">
